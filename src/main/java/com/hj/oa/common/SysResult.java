48ae94c81ffe963dc4a1f32710a4f1a2d4e6768b
hj08101 <2512613355@qq.com>
2018-07-31 07:48:46 +0800

    SysResult, Page基础组件封装

---
+++ src/main/java/com/hj/oa/common/SysResult.java
@@ -0,0 +1,37 @@
+package com.hj.oa.common;
+
+public class SysResult {
+	private boolean result;
+	private String data;
+
+	public boolean isResult() {
+		return result;
+	}
+
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public SysResult(boolean result, String data) {
+		super();
+		this.result = result;
+		this.data = data;
+	}
+
+	public SysResult(boolean result) {
+		this.result = result;
+	}
+
+	public SysResult() {
+		super();
+	}
+
+}
